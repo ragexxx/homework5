413277bde5c29f117a9831941ea0917f05453d19
Rigel Trujillo <rig_13@yahoo.com>
2021-04-16 11:16:27 -0500

    update files to newer version, fixes - v 2.0

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# homework5
+# Homework 5
+## Work Day Scheduler
+
+Work Day Scheduler is an application that allows to input tasks fromo 9am to 5pm that you can schedule your day every hour.
+
+
+- Add one task per hour
+- You can save your task and see it after you close the application
+- Different colors depending the time of the query
+
+## Preview
+
+The following image is a preview of the webpage:
+
+![Webpage URL:](image.PNG)
--- script.js
+++ script.js
@@ -1,2 +1,60 @@
 var today = moment();
+var options;
+var save9 = $('.saveBtn9');
+var save10 = $('.saveBtn10');
+var save11 = $('.saveBtn11');
+var save12 = $('.saveBtn12');
+var save13 = $('.saveBtn13');
+var save14 = $('.saveBtn14');
+var save15 = $('.saveBtn15');
+var save16 = $('.saveBtn16');
+var save17 = $('.saveBtn17');
+
 $("#currentDay").text(today.format("ddd MMM Do, YYYY"));
+
+getStorage();
+
+function getStorage(){
+    for( var i=9; i<=17; i++){
+        options = localStorage.getItem("hour"+i);
+        if(options != null){
+            $('#description' + i).val(options);
+        }
+    }
+}
+
+save9.on('click', function () {
+    localStorage.setItem( "hour9",$('textarea[name="description9"]').val() );
+  });
+
+save10.on('click', function () {
+    localStorage.setItem( "hour10",$('textarea[name="description10"]').val() );
+});
+
+save11.on('click', function () {
+    localStorage.setItem( "hour11",$('textarea[name="description11"]').val() );
+});
+
+save12.on('click', function () {
+    localStorage.setItem( "hour12",$('textarea[name="description12"]').val() );
+});
+
+save13.on('click', function () {
+    localStorage.setItem( "hour13",$('textarea[name="description13"]').val() );
+});
+
+save14.on('click', function () {
+    localStorage.setItem( "hour14",$('textarea[name="description14"]').val() );
+});
+
+save15.on('click', function () {
+    localStorage.setItem( "hour15",$('textarea[name="description15"]').val() );
+});
+
+save16.on('click', function () {
+    localStorage.setItem( "hour16",$('textarea[name="description16"]').val() );
+});
+
+save17.on('click', function () {
+    localStorage.setItem( "hour17",$('textarea[name="description17"]').val() );
+  });
